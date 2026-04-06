4efb1aa2b4d783b9d86b808a78ec8e7f47364e60
Jamshed Latipov <jamshedlatipov@gmail.com>
2026-04-06 18:57:24 +0500

    feat: Implement printer selection in PrintPreviewView and update font resources

--- ViewModels/PrintPreviewViewModel.cs
+++ ViewModels/PrintPreviewViewModel.cs
@@ -1,7 +1,29 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Win32;
 
 namespace LabelStudio.ViewModels;
 
 public partial class PrintPreviewViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private ObservableCollection<string> _printers = [];
+
+    [ObservableProperty]
+    private string? _selectedPrinter;
+
+    public PrintPreviewViewModel()
+    {
+        LoadPrinters();
+    }
+
+    private void LoadPrinters()
+    {
+        using var key = Registry.LocalMachine.OpenSubKey(
+            @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Printers");
+
+        var names = key?.GetSubKeyNames() ?? [];
+        Printers = new ObservableCollection<string>(names);
+        SelectedPrinter = Printers.Count > 0 ? Printers[0] : null;
+    }
 }
